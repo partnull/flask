5f5d8032603fde2d9ef20a5a82a36a3b4ccd18f9
partnull <65371432+partnull@users.noreply.github.com>
2020-05-20 23:06:25 +0100

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,1 +1,18 @@
+from app import app
+from flask import render_template
+import json
+from urllib.request import urlopen
+
+@app.route("/")
+def api_data():
+
+    url = "https://partnull.github.io/pizzapy/api.json"
+
+    api = json.loads(urlopen(url).read().decode("utf-8"))
+
+    result = api["articles"]
+    
+    data = result
+
+    return render_template("index.html", data=data)
 
